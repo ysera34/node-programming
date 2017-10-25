6233056f0c8c18f82e973bf5cb3e72fcdd04a50f
ysera34 <ycyoon34@gmail.com>
2017-10-25 21:29:38 +0900

    mime type

--- chapter06/home.js
+++ chapter06/home.js
@@ -5,7 +5,25 @@ http.createServer(function(req, res) {
    fs.readFile('home.html', function(error, data) {
        res.writeHead(200, {'Content-Type': 'text/html'});
        res.end(data);
-   }) 
+   });
+}).listen(52272, function() {
+    console.log('home server running at http://127.0.0.1:52272');
+});
+
+http.createServer(function(req, res) {
+    fs.readFile('sample.png', function(error, data) {
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        res.end(data);
+    });
+}).listen(52273, function() {
+    console.log('home server running at http://127.0.0.1:52273');
+});
+
+http.createServer(function(req, res) {
+    fs.readFile('sample.mp3', function(error, data) {
+        res.writeHead(200, {'Content-Type': 'audio/mp3'});
+        res.end(data);
+    });
 }).listen(52274, function() {
     console.log('home server running at http://127.0.0.1:52274');
 });
