eb94041e0682605c982bb61bbbd8b021a6428af9
ysera34 <ycyoon34@gmail.com>
2017-10-26 13:25:12 +0900

    response set cookie

--- chapter06/server.js
+++ chapter06/server.js
@@ -14,17 +14,27 @@ server.on('close', function() {
     console.log('Close On');
 });
 
-server.listen(52273, function () {
-    console.log('Server Running at http://127.0.0.1:52273');
-});
+//server.listen(52273, function () {
+//    console.log('Server Running at http://127.0.0.1:52273');
+//});
 
-setInterval(function () {
-    server.close();
-}, 3000);
+//setInterval(function () {
+//    server.close();
+//}, 3000);
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1> Hello Web Server with Node.js</h1>');
+    
+    var date = new Date();
+    date.setDate(date.getDate() + 7);
+    
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Set-Cookie': [
+            'breakfast = toast; Expires = ' + date.toUTCString(),
+            'dinner = chicken'
+        ]
+    });
+    res.end('<h1> Hello Web Server with Node.js</h1><h2>' + req.headers.cookie + '</h2>');
 }).listen(52274, function() {
     console.log('Server Running at http://127.0.0.1:52274');
 })
