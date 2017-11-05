27f7033a9b044156eac514bbc4539ca091e37343
ysera34 <ycyoon34@gmail.com>
2017-11-05 23:43:06 +0900

    express response method

--- chapter08/app.js
+++ chapter08/app.js
@@ -4,8 +4,18 @@ var express = require('express');
 var app = express();
 
 app.use(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html'});
-    res.end('<h1>Hello Express</h1>');
+    
+    var output = [];
+    for (var i = 0; i < 3; i++) {
+        output.push({
+            count: i,
+            name: 'name - ' + i
+        })
+    }
+    res.send(output);
+    
+//    res.writeHead(200, { 'Content-Type': 'text/html'});
+//    res.end('<h1>Hello Express</h1>');
 });
 
 http.createServer(app).listen(52273, function() {
