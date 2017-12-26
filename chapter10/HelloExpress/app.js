7aa6f4448828d9d046837597ac8a16928243bb59
ysera34 <ycyoon34@gmail.com>
2017-12-26 22:20:33 +0900

    page routes

--- chapter10/HelloExpress/app.js
+++ chapter10/HelloExpress/app.js
@@ -14,6 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set('case sensitive routes', true);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,8 +24,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', index);
 app.use('/users', users);
+app.use('/product', function(req, res) {
+  res.render('product', { title: 'Product Page' });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
